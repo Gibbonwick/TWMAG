17bab7d2814a10713070213d8efbaf43be72ba8d
Brilson <45705197+Brilson@users.noreply.github.com>
2020-08-12 03:02:48 +0900

    Pause Menu
    Added a Pause Menu that is opened with the ESC key and has options to Resume, Restart, and Exit.
    
    *Notes*
    - The exit button only works in builds and not in the editor
    - Gameplay is paused when the menu opens, but button hover interactions still trigger.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject MenuScreen;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        MenuScreen.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        MenuScreen.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    //Only works in build, not in the editor
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
